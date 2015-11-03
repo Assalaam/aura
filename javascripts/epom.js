67bea05d27694af8e340358a6e7526135e456ad1
Assalaam <atas_nama_kasihsayang@yahoo.com>
2015-11-03 21:14:09 +0700

    Delete epom.js

--- javascripts/epom.js
+++
@@ -1,8 +0,0 @@
-epmads_key = "a1e1751dc271bf554b9cd9a46bc0b5c1";
-epmads_channel = "";
-epmads_code_format = "ads-sync.js";
-epmads_ads_host = "//ecpmrocks.com";
-epmads_click = "";
-epmads_custom_params = {};
-
-document.write("<script type='text\/javascript' src='"+(location.protocol == 'https:' ? 'https:' : 'http:') + "//ecpmrocks.com\/js/show_ads_epmads.js?pubId=12400'><\/script>");
