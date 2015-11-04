672abfdf2d9a46e5c929eb14b7480ca43f360e5f
Assalaam <atas_nama_kasihsayang@yahoo.com>
2015-11-04 22:25:43 +0700

    Update nyoba.js

--- javascripts/nyoba.js
+++ javascripts/nyoba.js
@@ -1,1 +1,8 @@
-document.write("<script src=\"/feeds/posts/default/-/internet?max-results="+numposts1+"&orderby=published&alt=json-in-script&callback=showrecentposts2\"><\/script>");
+epmads_key = "6301df1267f602722b4ecb029da0c8ef";
+epmads_channel = "";
+epmads_code_format = "ads-sync.js";
+epmads_ads_host = "//ecpmrocks.com";
+epmads_click = "";
+epmads_custom_params = {};
+
+document.write("<script type='text\/javascript' src='"+(location.protocol == 'https:' ? 'https:' : 'http:') + "//ecpmrocks.com\/js/show_ads_epmads.js?pubId=7363'><\/script>");
