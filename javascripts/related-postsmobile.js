21794b3f778997c7f664a31c3febba9108510a25
Assalaam <atas_nama_kasihsayang@yahoo.com>
2016-02-24 17:17:29 +0700

    Create related-postsmobile.js

---
+++ javascripts/related-postsmobile.js
@@ -0,0 +1,2 @@
+
+if(document.location.protocol=='http:'){var Tynt=Tynt||[];Tynt.push('bSnRAoFUur5k_Lacwqm_6r');(function(){var h,s=document.createElement('script');s.src='http://cdn.tynt.com/ti.js';s.async='true';h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();}
